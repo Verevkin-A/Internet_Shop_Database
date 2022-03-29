46a99ec633c9ad884a74f79e72e766fe3dd16cfc
Verevkin-A <sandyverevkin@gmail.com>
2022-03-29 20:45:25 +0200

    fix: add missing values && tables dependencies

--- Readme.md
+++ Readme.md
@@ -1,6 +1,7 @@
 # Scripts for working with online shop database.
 
 # TODO's:
+## We dont need products string when we have cart-products relation-table. Think about it
 - something wrong with auto generated primary keys? dont know yet what exactly, but will think about it
 - generalization
 - IVAN check foreign keys
--- xverev00_xtsiar00.sql
+++ xverev00_xtsiar00.sql
@@ -9,14 +9,6 @@ DROP TABLE review CASCADE CONSTRAINTS PURGE;
 DROP TABLE cart_product CASCADE CONSTRAINTS PURGE;
 DROP TABLE order_product CASCADE CONSTRAINTS PURGE;
 
---Create cart table
-CREATE TABLE cart (
-    cart_id       INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    products      VARCHAR(256) DEFAULT NULL,
-    total_price   INT DEFAULT 0 NOT NULL
-        CHECK(total_price >= 0)
-);
-
 --Create customer table
 CREATE TABLE customer (
     customer_id   INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -33,12 +25,20 @@ CREATE TABLE customer (
     telephone_num NUMERIC(15, 0) NOT NULL
         CHECK (telephone_num > 99999999),
     email         VARCHAR(256) UNIQUE NOT NULL
-        CHECK (REGEXP_LIKE (email, '^[a-zA-Z0-9.!#$%&*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-]+$')),
+        CHECK (REGEXP_LIKE (email, '^[a-zA-Z0-9.!#$%&*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-]+$'))
+);
+
+--Create cart table
+CREATE TABLE cart (
+    cart_id       INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    products      VARCHAR(256) DEFAULT NULL,    --TODO remove?
+    total_price   INT DEFAULT 0 NOT NULL
+        CHECK(total_price >= 0),
     -- foreign keys
-    cart INT DEFAULT NULL,
-    CONSTRAINT customer_cart_foreign
-        FOREIGN KEY (cart)
-        REFERENCES cart (cart_id)
+    customer      INT DEFAULT NULL,
+    CONSTRAINT cart_customer_foreign
+        FOREIGN KEY (customer)
+        REFERENCES customer (customer_id)
         ON DELETE CASCADE
 );
 
@@ -59,14 +59,16 @@ CREATE TABLE "ORDER" (
     invoice       INT UNIQUE NOT NULL
         CHECK (invoice > 0),
     --foreign keys
-    employee INT DEFAULT NULL,
-    customer INT DEFAULT NULL,
+    employee      INT DEFAULT NULL,
+    customer      INT DEFAULT NULL,
     CONSTRAINT order_employee_foreign
         FOREIGN KEY (employee)
-        REFERENCES employee (employee_id),
+        REFERENCES employee (employee_id)
+        ON DELETE SET NULL,
     CONSTRAINT order_customer_foreign
         FOREIGN KEY (customer)
         REFERENCES customer (customer_id)
+        ON DELETE CASCADE
 );
 
 --Create supplier table
@@ -90,14 +92,16 @@ CREATE TABLE product (
     price         NUMERIC(3, 0) NOT NULL
         CHECK (price >= 0),
     --foreign keys
-    supplier INT DEFAULT NULL,
-    employee INT DEFAULT NULL,
+    supplier      INT DEFAULT NULL,
+    employee      INT DEFAULT NULL,
     CONSTRAINT product_supplier_foreign
         FOREIGN KEY (supplier)
-        REFERENCES supplier (supplier_id),
+        REFERENCES supplier (supplier_id)
+        ON DELETE CASCADE,
     CONSTRAINT product_employee_foreign
         FOREIGN KEY (employee)
         REFERENCES employee (employee_id)
+        ON DELETE SET NULL
 );
 
 --Create review table
@@ -108,45 +112,51 @@ CREATE TABLE review (
                     CHECK (rating >= 0 AND rating <= 5),
      content      VARCHAR(256),
     --foreign keys
-    customer INT DEFAULT NULL,
-    product INT DEFAULT NULL,
+    customer      INT DEFAULT NULL,
+    product       INT DEFAULT NULL,
     CONSTRAINT review_customer_foreign
         FOREIGN KEY (customer)
-        REFERENCES customer (customer_id),
+        REFERENCES customer (customer_id)
+        ON DELETE CASCADE,
     CONSTRAINT review_product_foreign
         FOREIGN KEY (product)
         REFERENCES product (product_id)
+        ON DELETE CASCADE
 );
 
 --Cart-Product relation table
 CREATE TABLE cart_product (
-	cart INT NOT NULL,
-	product INT NOT NULL,
+	cart          INT NOT NULL,
+	product       INT NOT NULL,
 	CONSTRAINT cart_product_primary
 		PRIMARY KEY (cart, product),
 	CONSTRAINT cart_product_cart_foreign
-		FOREIGN KEY (cart) REFERENCES cart (cart_id),
+		FOREIGN KEY (cart)
+		REFERENCES cart (cart_id)
+        ON DELETE CASCADE,
 	CONSTRAINT cart_product_product_foreign
-		FOREIGN KEY (product) REFERENCES product (product_id)
+		FOREIGN KEY (product)
+		REFERENCES product (product_id)
+        ON DELETE CASCADE
 );
 
 --Order-Product relation table
 CREATE TABLE order_product (
-	"order" INT NOT NULL,
-	product INT NOT NULL,
+	"order"       INT NOT NULL,
+	product       INT NOT NULL,
 	CONSTRAINT order_product_primary
 		PRIMARY KEY ("order", product),
 	CONSTRAINT order_product_order_foreign
-		FOREIGN KEY ("order") REFERENCES "ORDER" (order_id)
+		FOREIGN KEY ("order")
+		REFERENCES "ORDER" (order_id)
 		ON DELETE CASCADE,
 	CONSTRAINT order_product_product_foreign
-		FOREIGN KEY (product) REFERENCES product (product_id)
-		ON DELETE CASCADE
+		FOREIGN KEY (product)
+		REFERENCES product (product_id)
+        ON DELETE CASCADE
 );
 
 --Insert values into created tables
-INSERT INTO cart(products, total_price) VALUES(DEFAULT, DEFAULT);
-INSERT INTO cart(products, total_price) VALUES('1 Black crayon Bulk, 2x A4 sketchbook Conda', 31);
 
 INSERT INTO customer(name, surname, birthdate, password, street, city, zip_code, payment_info, telephone_num, email)
 VALUES('Michael', 'Murphy', TO_DATE('07/05/1976', 'DD/MM/YYYY'), '11002003001', '9 Whitney', 'Smithtown', '117817',
@@ -161,33 +171,40 @@ INSERT INTO customer(name, surname, birthdate, password, street, city, zip_code,
 VALUES('James', 'Taylor', TO_DATE('01/01/1991', 'DD/MM/YYYY'), '2j2@ks2l', 'Blackhorse Grove 118','London', '21345',
        1232933677494098, 420601228345, 'taylor@gmail.com');
 
+INSERT INTO cart(products, total_price, customer) VALUES(DEFAULT, DEFAULT, 1);
+INSERT INTO cart(products, total_price, customer) VALUES('1 Black crayon Bulk, 2x A4 sketchbook Conda', 31, 2);
+INSERT INTO cart(products, total_price, customer) VALUES('1 White crayon Bulk', 7, 3);
+INSERT INTO cart(products, total_price, customer) VALUES('1x A4 sketchbook Conda', 12, 4);
+
 INSERT INTO employee(name, surname, password) VALUES('Jack', 'Wilson', 'DOsmi42@s');
 INSERT INTO employee(name, surname, password) VALUES('Sophie', 'Rodriguez', 'qkjw11234j');
 
-INSERT INTO "ORDER"(invoice) VALUES(2491839);
-INSERT INTO "ORDER"(invoice) VALUES(202021166);
-INSERT INTO "ORDER"(status, invoice) VALUES('PROCESSED', 03000852021);
-INSERT INTO "ORDER"(status, invoice) VALUES('DELIVERING', 1900001);
-INSERT INTO "ORDER"(status, invoice) VALUES('DELIVERED', 1352);
+INSERT INTO "ORDER"(invoice, employee, customer) VALUES(2491839, 2, 1);
+INSERT INTO "ORDER"(invoice, employee, customer) VALUES(202021166, 1, 1);
+INSERT INTO "ORDER"(status, invoice, employee, customer) VALUES('PROCESSED', 03000852021, 1, 2);
+INSERT INTO "ORDER"(status, invoice, employee, customer) VALUES('DELIVERING', 1900001, 1, 3);
+INSERT INTO "ORDER"(status, invoice, employee, customer) VALUES('DELIVERED', 1352, 2, 4);
 
 INSERT INTO supplier(supplier_name, street, city, zip_code, company_ID, VAT_ID)
 VALUES('Office SUP s.r.o', '59a Commercial St', 'Rothwell', '83294', '09354970', 'CZ123456789');
+INSERT INTO supplier(supplier_name, street, city, zip_code, company_ID, VAT_ID)
+VALUES('BestSupplies s.r.o', '23b Supplies St', 'Bestcity', '42000', '83720420', 'CZ847205730');
 
-INSERT INTO product(product_name, description, price)
-VALUES('Black crayon Bulk', 'Black Long-Lasting Marking Crayon', 7);
-INSERT INTO product(product_name, description, price)
-VALUES('White crayon Bulk', 'White Long-Lasting Marking Crayon', 7);
-INSERT INTO product(product_name, description, price)
-VALUES('Red crayon Bulk', 'Red Long-Lasting Marking Crayon', 7);
-INSERT INTO product(product_name, description, price)
-VALUES('A4 sketchbook Conda', 'A4 Heavyweight Hardcover Sketchbook, Ideal for Kids & Adults', 12);
-INSERT INTO product (product_name, description, price)
-VALUES('A6 sketchbook Conda', 'A3 Heavyweight Hardcover Sketchbook, Ideal for Kids & Adults', 9);
+INSERT INTO product(product_name, description, price, supplier, employee)
+VALUES('Black crayon Bulk', 'Black Long-Lasting Marking Crayon', 7, 2, 1);
+INSERT INTO product(product_name, description, price, supplier, employee)
+VALUES('White crayon Bulk', 'White Long-Lasting Marking Crayon', 7, 2, 2);
+INSERT INTO product(product_name, description, price, supplier, employee)
+VALUES('Red crayon Bulk', 'Red Long-Lasting Marking Crayon', 7, 1, 1);
+INSERT INTO product(product_name, description, price, supplier, employee)
+VALUES('A4 sketchbook Conda', 'A4 Heavyweight Hardcover Sketchbook, Ideal for Kids & Adults', 12, 1, 2);
+INSERT INTO product (product_name, description, price, supplier, employee)
+VALUES('A6 sketchbook Conda', 'A3 Heavyweight Hardcover Sketchbook, Ideal for Kids & Adults', 9, 2, 1);
 
-INSERT INTO review(rating, content)
-VALUES(5, 'Thanks for awesome A4 sketchbook!');
-INSERT INTO review(rating, content)
-VALUES(2, NULL);
+INSERT INTO review(rating, content, customer, product)
+VALUES(5, 'Thanks for awesome A4 sketchbook!', 2, 3);
+INSERT INTO review(rating, content, customer, product)
+VALUES(2, NULL, 4, 1);
 
 INSERT INTO cart_product(cart, product) VALUES (1, 1);
 INSERT INTO cart_product(cart, product) VALUES (1, 3);
